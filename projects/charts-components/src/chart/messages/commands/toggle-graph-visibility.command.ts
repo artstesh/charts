18525509f2c7c74d89ccbd3d7fd3fda8248e1174
artstesh <artstesh@mail.ru>
2024-11-20 13:12:04 +0300

    5.2.1

--- projects/charts-components/src/chart/messages/commands/toggle-graph-visibility.command.ts
+++ projects/charts-components/src/chart/messages/commands/toggle-graph-visibility.command.ts
@@ -1,9 +1,9 @@
-import { PostboyGenericMessage } from "@artstesh/postboy";
+import { PostboyGenericMessage } from '@artstesh/postboy';
 
 /**
  * Represents a command to toggle the visibility of an element of a chart.
  */
-export class ToggleGraphVisibilityCommand extends PostboyGenericMessage{
+export class ToggleGraphVisibilityCommand extends PostboyGenericMessage {
   public static readonly ID = '10f920e6-64e0-47b5-bc76-d9bcf6b2f89c';
   public get id(): string {
     return ToggleGraphVisibilityCommand.ID;
