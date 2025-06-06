bb3af94791cf9c051a428279a2cac4f6ba474cb9
artstesh <artstesh@mail.ru>
2025-06-06 10:58:58 +0300

    v5

--- projects/charts-components/src/chart/services/inner-postboy.service.ts
+++ projects/charts-components/src/chart/services/inner-postboy.service.ts
@@ -2,4 +2,8 @@ import { Injectable } from '@angular/core';
 import { PostboyService } from '@artstesh/postboy';
 
 @Injectable()
-export class InnerPostboyService extends PostboyService {}
+export class InnerPostboyService extends PostboyService {
+  constructor() {
+    super();
+  }
+}
