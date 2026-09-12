fda8f0c9cd6fc7b55ab142c92e02ff2cb0173517
artstesh <artstesh@mail.ru>
2026-09-12 11:38:12 +0300

    Port from v9 (000cc42): upgrade to @artstesh/postboy 3.3.0 and update related dependencies and usages.
    Specs moved from the removed PostboyServiceMock API to PostboyWorld; framework packages moved to peerDependencies; GetGradientExecutor result asserted non-null.

--- projects/charts-components/src/chart/chart-plate/chart-types/area-chart/area-chart.component.ts
+++ projects/charts-components/src/chart/chart-plate/chart-types/area-chart/area-chart.component.ts
@@ -119,7 +119,7 @@ export class AreaChartComponent extends AbstractChartTypeComponent<AreaChartSett
       new BuildAreaChartExecutor(
         this._settings,
         this._data,
-        this.postboy.exec(new GetGradientExecutor(this.chart ?? null, this._settings.colors, this._settings.direction)),
+        this.postboy.exec(new GetGradientExecutor(this.chart ?? null, this._settings.colors, this._settings.direction))!,
       ),
     );
     this.alsoDelete = () => content.bottom?.id;
